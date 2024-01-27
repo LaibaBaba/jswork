337b37c214d901aecec949951a2a639c6971d09a
LaibaBaba <145117819+LaibaBaba@users.noreply.github.com>
2024-01-27 17:15:00 +0000

    datatypes

---
+++ 01_basics/03_datatypes_Summary.js
@@ -0,0 +1,16 @@
+//Array
+const hero = ["bilal abbas, sana khan, imran khan"]
+
+
+//objects
+let myojb = {
+    Name : "laiba",
+    Class : 14,
+    age : 22
+}
+
+
+//function
+let myfunction = function(){
+    console.log("hello world");
+}
